315ea6d71f328e6281a0a59dda97b162deb43e01
MargotvanM <76996904+MargotvanM@users.noreply.github.com>
2021-09-22 10:50:02 +0200

    Next steps, not complete yet

--- Paretoscaling.py
+++ Paretoscaling.py
@@ -1,2 +1,14 @@
 # Paretoscaling
+import pandas as pd
+import numpy as np
+import math
 
+#df =                             #some sort of dataframe
+
+mean_vector = df.mean(axis=1)
+mean_centering = np.devide(df, mean_vector)
+
+std_vector = np.std(df)
+root_std = math.sqrt(std_vector)
+
+scaled_data = np.devide(mean_centering, root_std)
